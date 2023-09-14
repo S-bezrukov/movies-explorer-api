7ac7a5a2c0d933eebd32adb447314688e18e7bfe
Sergey Bezrukov <sb700sb@yandex.ru>
2023-09-14 14:02:48 +0300

    Сделать весь необходимый функционал

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {
+  updateProfile,
+  getMe,
+} = require('../controllers/users');
+const { updateProfileValidator } = require('../utils/validators/userValidator');
+
+router.get('/me', getMe);
+router.patch('/me', updateProfileValidator, updateProfile);
+
+module.exports = router;
